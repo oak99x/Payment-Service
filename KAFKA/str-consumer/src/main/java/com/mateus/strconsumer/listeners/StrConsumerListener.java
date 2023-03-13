8b5c439e61db711329285c163819e2603338d75f
Mateus <mateus.oak99@gmail.com>
2023-03-13 15:50:33 -0300

    refactor(KafkaListener): create  StrConsumerCustomListener

--- KAFKA/str-consumer/src/main/java/com/mateus/strconsumer/listeners/StrConsumerListener.java
+++ KAFKA/str-consumer/src/main/java/com/mateus/strconsumer/listeners/StrConsumerListener.java
@@ -1,7 +1,10 @@
 package com.mateus.strconsumer.listeners;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
+
+import com.mateus.strconsumer.custom.StrConsumerCustomListener;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -9,13 +12,18 @@ import lombok.extern.log4j.Log4j2;
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message) {
         log.info("CREATE ::: Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message) {
         log.info("LOG ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive message {}", message);
     }
 }
