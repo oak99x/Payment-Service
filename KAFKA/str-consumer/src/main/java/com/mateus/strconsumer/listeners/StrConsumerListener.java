fe56e8937fbd34d8e667c14032d58f2d7c4e583c
Mateus <mateus.oak99@gmail.com>
2023-03-13 14:57:17 -0300

    feat(str-consumer): new feature consumer

--- KAFKA/str-consumer/src/main/java/com/mateus/strconsumer/listeners/StrConsumerListener.java
+++ KAFKA/str-consumer/src/main/java/com/mateus/strconsumer/listeners/StrConsumerListener.java
@@ -10,7 +10,12 @@ import lombok.extern.log4j.Log4j2;
 public class StrConsumerListener {
 
     @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Receive message {}", message);
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message);
+    }
+
+    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message);
     }
 }
